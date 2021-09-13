898fbfd593e2122c4a5d017eb9657f44115b2156
YongKhyeShern <90166518+YongKhyeShern@users.noreply.github.com>
2021-09-13 11:05:37 +0800

    Add files via upload

---
+++ Lab 2.3.py
@@ -0,0 +1,6 @@
+#Student ID: 1201201010
+#Student Name: Yong Khye Shern
+
+celsius=float(input("Please enter the value of Celsius "))
+fahrenheit=(celsius*1.8)+32
+print("For",celsius,"°C is equivalent to ",fahrenheit,"°F")
